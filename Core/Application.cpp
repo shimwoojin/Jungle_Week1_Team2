3163b4c91133855ff23630121a6cc948ea6942f0
Yonaim <yonaimyo@gmail.com>
2026-03-10 22:43:46 +0900

    Refactor: App의 초기화에서 SceneManager는 init만 수행

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -1,123 +1,117 @@
-#include "pch.h"
 #include "Application.h"
 #include "Data/StageLoader.h"
+#include "IO/ImageLoader.h"
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_dx11.h"
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_internal.h"
-#include "IO/ImageLoader.h"
-
+#include "pch.h"
 
 bool FApplication::Initialize(HINSTANCE hInstance, int ScreenWidth, int ScreenHeight)
 {
-	if (!Window.Initialize(hInstance, ScreenWidth, ScreenHeight, L"My Game"))
-		return false;
+    if (!Window.Initialize(hInstance, ScreenWidth, ScreenHeight, L"My Game"))
+        return false;
 
-	HWND WindowHandle = Window.GetHandle();
-	if (WindowHandle == nullptr)
-		return false;
+    HWND WindowHandle = Window.GetHandle();
+    if (WindowHandle == nullptr)
+        return false;
 
-	Time = std::make_unique<FTime>();
-	Input = std::make_unique<FInput>();
-	Renderer = std::make_unique<FRenderer>();
-	TextureManager = std::make_unique<FTextureManager>();
-	SceneManager = std::make_unique<FSceneManager>();
+    Time = std::make_unique<FTime>();
+    Input = std::make_unique<FInput>();
+    Renderer = std::make_unique<FRenderer>();
+    TextureManager = std::make_unique<FTextureManager>();
+    SceneManager = std::make_unique<FSceneManager>();
 
-	if (!Renderer->Initialize(WindowHandle, ScreenWidth, ScreenHeight))
-		return false;
+    if (!Renderer->Initialize(WindowHandle, ScreenWidth, ScreenHeight))
+        return false;
 
-	//TEST Register
+    // TEST Register
 
-	auto LoadTex = [&](const std::string& Key, const std::string& Path)
-		{
-			if (!TextureManager->Has(Key))
-			{
-				auto Tex = FImageLoader::LoadAsTexture(Renderer->Device, Path);
-				if (Tex)
-					TextureManager->Register(Key, std::move(Tex));
-			}
-		};
-	LoadTex("tile_floor", "Resources/Sprites/tile_floor.png");
-	LoadTex("goal", "Resources/Sprites/goal.png");
-	LoadTex("wall", "Resources/Sprites/wall.png");
-	LoadTex("player", "Resources/Sprites/player.png");
-	LoadTex("monster", "Resources/Sprites/monster.png");
+    auto LoadTex = [&](const std::string &Key, const std::string &Path)
+    {
+        if (!TextureManager->Has(Key))
+        {
+            auto Tex = FImageLoader::LoadAsTexture(Renderer->Device, Path);
+            if (Tex)
+                TextureManager->Register(Key, std::move(Tex));
+        }
+    };
+    LoadTex("tile_floor", "Resources/Sprites/tile_floor.png");
+    LoadTex("goal", "Resources/Sprites/goal.png");
+    LoadTex("wall", "Resources/Sprites/wall.png");
+    LoadTex("player", "Resources/Sprites/player.png");
+    LoadTex("monster", "Resources/Sprites/monster.png");
 
+    // TODO
+    // TextureManager->Initialize(Renderer->Device);
 
+    // 스테이지 데이터 로드
+    FStageLoader::Get().Initialize("Resources/Maps/stages.json");
 
+    // ImGui 초기화
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplWin32_Init(WindowHandle);
+    ImGui_ImplDX11_Init(Renderer->Device, Renderer->DeviceContext);
 
-	// TODO
-	// TextureManager->Initialize(Renderer->Device);
+    GameContext.emplace(FGameContext{*Time, *Input, *Renderer, *TextureManager});
+    SceneManager->Initialize();
 
-	// 스테이지 데이터 로드
-	FStageLoader::Get().Initialize("Resources/Maps/stages.json");
-
-	// ImGui 초기화
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui::StyleColorsDark();
-	ImGui_ImplWin32_Init(WindowHandle);
-	ImGui_ImplDX11_Init(Renderer->Device, Renderer->DeviceContext);
-
-	GameContext.emplace(FGameContext{ *Time, *Input, *Renderer, *TextureManager });
-
-	SceneManager->Initialize(&GameContext.value());
-	SceneManager->ChangeSceneImmediately(ESceneType::Title);
-
-	return true;
+    return true;
 }
 
 /*
-		1. Input 처리
-		2. Time 처리 (실제 흐른 시각 기준)
-		3. Scene 업데이트: Scene 타입을 구분하여 처리
-		4. Scene 렌더
+    1. Input 처리
+    2. Time 처리 (실제 흐른 시각 기준)
+    3. Scene 업데이트: Scene 타입을 구분하여 처리
+    4. Scene 렌더
 */
 void FApplication::Run()
 {
-	while (bIsRunning && Window.ProcessMessages())
-	{
-		// Input, Time 업데이트
-		Input->Update();
-		Time->Update();
+    while (bIsRunning && Window.ProcessMessages())
+    {
+        // Input, Time 업데이트
+        Input->Update();
+        Time->Update();
 
-		// 프레임 렌더 준비
-		ImGui_ImplDX11_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-		ImGui::NewFrame();
+        // 프레임 렌더 준비
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
 
-		// Scene 업데이트
-		SceneManager->Update();
+        // Scene 업데이트
+        SceneManager->Update(*GameContext);
 
-		// Scene 렌더
-		Renderer->BeginFrame();
-		SceneManager->Render();
-		ImGui::Render();
-		Renderer->Render();
-		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-		Renderer->EndFrame();
-	}
+        // Scene 렌더
+        Renderer->BeginFrame();
+        SceneManager->Render(*GameContext);
+        ImGui::Render();
+        Renderer->Render();
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+        Renderer->EndFrame();
+    }
 }
 
 void FApplication::Shutdown()
 {
-	ImGui_ImplDX11_Shutdown();
-	ImGui_ImplWin32_Shutdown();
-	ImGui::DestroyContext();
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
 
-	Window.Shutdown();
+    Window.Shutdown();
 }
 
 void FApplication::RequestQuit() { bIsRunning = false; }
 
 bool FApplication::IsRunning() const { return bIsRunning; }
 
-FTime& FApplication::GetTime() { return *Time; }
+FTime &FApplication::GetTime() { return *Time; }
 
-FInput& FApplication::GetInput() { return *Input; }
+FInput &FApplication::GetInput() { return *Input; }
 
-FRenderer& FApplication::GetRenderer() { return *Renderer; }
+FRenderer &FApplication::GetRenderer() { return *Renderer; }
 
-FTextureManager& FApplication::GetTextureManager() { return *TextureManager; }
+FTextureManager &FApplication::GetTextureManager() { return *TextureManager; }
 
-FSceneManager& FApplication::GetSceneManager() { return *SceneManager; }
+FSceneManager &FApplication::GetSceneManager() { return *SceneManager; }
