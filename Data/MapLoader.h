eba2b9bbb8db222db34874ebb0c03a519df12ed4
Yonaim <yonaimyo@gmail.com>
2026-03-10 13:46:55 +0900

    Feat: MapLoader 구현

--- Data/MapLoader.h
+++ Data/MapLoader.h
@@ -6,7 +6,13 @@ class FMapData;
 
 class FMapLoader
 {
-public:
-	bool LoadFromFile(const std::string& Path, FMapData& OutMap);
-	bool SaveToFile(const std::string& Path, const FMapData& Map);
+  public:
+    bool LoadFromFile(FMapData &OutMap); // legacy, 삭제 예정
+    bool LoadStageById(int StageId, FMapData &OutMap);
+    int  GetStageCount();
+
+  private:
+    // TODO: 경로 외부 지정 혹은 상수로 빼두기
+    static constexpr const char *DefaultPath = "Assets/Data/Maps.json";
+    std::string                  Path = DefaultPath;
 };
